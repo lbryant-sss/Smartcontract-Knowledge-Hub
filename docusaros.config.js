22410ed25b4841fadb1da70ed862303959f5bde3
JustBrian <61902485+lbryant-sss@users.noreply.github.com>
2025-04-24 19:32:04 +0300

    Config Added

--- docusaros.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  title: 'Smart Contract Hacking - Mungai',
-  url: 'https://lbryant-sss.github.io',
-  baseUrl: '/mungai/', // your repo name
-  organizationName: 'lbryant-sss', // GitHub username
-  projectName: 'mungai', // repo name
-  ...
-};
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,12 +10,15 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Smart Contract Security',
+  tagline: 'Knowledge Hub',
   favicon: 'img/favicon.ico',
-
+url: 'https://lbryant-sss.github.io',
+baseUrl: '/mungai/',
+organizationName: 'lbrant-sss',
+projectName: 'Smartcontract-Knowledge-Hub',
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+//  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
